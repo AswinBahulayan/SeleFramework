96a0780cf83a4e181fd0aa06f6202263fb78ce18
AswinBahulayan <aswinbahulayan@gmail.com>
2018-08-27 20:42:16 +0530

    project method added

---
+++ src/main/java/org/testcases/pack/ProjectMethod.java
@@ -0,0 +1,41 @@
+package org.testcases.pack;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebElement;
+import org.selenium.helper.com.SeleniumHelper;
+import org.testdatamanager.com.Getdata;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+
+
+public class ProjectMethod extends SeleniumHelper {
+	
+	@DataProvider(name="getdata")
+	public Object[][] data() throws IOException
+	{
+		Object[][] getdata = Getdata.getdata(excelfilename);
+		
+		return getdata;
+	}
+
+	@BeforeMethod
+	public  void login()
+	{
+		startApp("chrome", "http://leaftaps.com/opentaps");
+		WebElement eleUserName = locateElement("id", "username");
+		type(eleUserName, "DemoSalesManager");
+		WebElement elePassword = locateElement("id","password");
+		type(elePassword, "crmsfa");
+		//WebElement eleLogin = locateElement("class","decorativeSubmit");
+		click(locateElement("//form[@id='login']/child::p/input[@type='submit']"));
+		//WebElement crmsfa = locateElement("LinkText", "CRM/SFA");
+		click(locateElement( "(//div[@id='button']/child::a/img)[1]"));
+	}
+	
+
+}
